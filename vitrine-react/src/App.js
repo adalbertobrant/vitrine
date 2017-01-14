d0b752489f2963ced6f64b384dfe12532d66603b
Julio Faerman <jfaerman@gmail.com>
2017-01-14 12:36:59 -0200

    Initial React + Material UI project

--- vitrine-react/src/App.js
+++ vitrine-react/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <MyAwesomeReactComponent/>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
---
+++ vitrine-react/src/MyAwesomeReactComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const MyAwesomeReactComponent = () => (<RaisedButton label="UALA"/>);
+
+export default MyAwesomeReactComponent;
